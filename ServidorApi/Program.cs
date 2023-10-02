0b252c17fb2968b7aa455b64d53576b64e5f4ce7
nachog86 <gomezignacio86@gmail.com>
2023-10-02 12:10:06 -0300

    cambios en el server,  con cloudinary y ase de datos

--- ServidorApi/Program.cs
+++ ServidorApi/Program.cs
@@ -1,16 +1,29 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using CloudinaryDotNet;
-using CloudinaryDotNet.Actions;
-
+using DotNetEnv;
+using ServidorApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Cargar variables de entorno del archivo .env
+Env.Load();
+
+// Configuración de Cloudinary
+var cloudinary = new Cloudinary();  // Esto usará CLOUDINARY_URL del entorno automáticamente
+builder.Services.AddSingleton(cloudinary);
+
+// Configuración de Entity Framework y PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING"))
+);
+
+// Otros servicios
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -28,4 +41,3 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
-
